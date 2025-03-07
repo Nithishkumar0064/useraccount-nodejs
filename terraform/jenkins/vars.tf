35375a9b85c4d8c185f589ff14db829b69ea2035
HariGangaiah <harigangaiah@gmail.com>
2025-03-07 13:49:17 +0530

    Add files via upload

---
+++ terraform/jenkins/vars.tf
@@ -0,0 +1,47 @@
+variable "region" {
+  description = "Region where the resources will be created"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "jenkins_master_ami" {
+  description = "AMI for Jenkins Master"
+  type        = string
+  default     = "ami-00bb6a80f01f03502"
+}
+
+variable "jenkins_agent_ami" {
+  description = "AMI for Jenkins Agent"
+  type        = string
+  default     = "ami-00bb6a80f01f03502" 
+}
+
+variable "jenkins_master_instance_type" {
+  description = "Instance type for Jenkins Master and Agent"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "jenkins_agent_instance_type" {
+  description = "Instance type for Jenkins Master and Agent"
+  type        = string
+  default     = "t2.medium"
+}
+
+variable "key_name" {
+  description = "Key pair name for SSH access"
+  type        = string
+  default     = "Sonar"
+}
+
+variable "jenkins_master_name" {
+  description = "Name tag for Jenkins Master instance"
+  type        = string
+  default     = "Jenkins-Master"
+}
+
+variable "jenkins_agent_name" {
+  description = "Name tag for Jenkins Agent instance"
+  type        = string
+  default     = "Jenkins-Agent"
+}
